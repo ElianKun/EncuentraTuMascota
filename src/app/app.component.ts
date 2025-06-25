d99ce31272bbdd5e28c50ed247677d5184f582b5
Agustin <agustinfernandez43tb@gmail.com>
2025-06-24 23:33:19 -0300

    chore: generar componentes base (navbar, footer, pagina principal) y agregar ngx-slick-carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaginaPrincipalComponent } from "./Componentes/pagina-principal/pagina-principal.component";
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { NavbarComponent } from "./Componentes/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PaginaPrincipalComponent, SlickCarouselModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
